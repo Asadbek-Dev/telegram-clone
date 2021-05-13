1a16cebb6e3be77d4a2c17c39d019175c71d01e6
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-05-13 23:27:17 +0500

    added user info

--- src/components/SidebarThread.js
+++ src/components/SidebarThread.js
@@ -1,15 +1,42 @@
 import { Avatar } from '@material-ui/core';
 import './SiderbarThread.css'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux';
+import db from '../firebase';
+import { setThread } from '../features/threadSlice'
 
-const SidebarThread = () => {
+const SidebarThread = ({ id, threadName }) => {
+    const dispatch = useDispatch();
+    const [threadInfo, setThreadInfo] = useState([]);
+
+    useEffect(() => {
+        db.collection('threads')
+            .doc(id)
+            .collection('messages')
+            .orderBy('timestamp', 'desc')
+            .onSnapshot((snapshot) =>
+                setThreadInfo(snapshot.docs.map((doc) => doc.data()))
+            )
+    }, [id]);
+
+
+
     return (
-        <div className='sidebarThread'>
-            <Avatar />
+        <div className='sidebarThread'
+            onClick={() => dispatch(setThread({
+                thread: id,
+                threadName: threadName
+            }))}
+        >
+            <Avatar src={threadInfo[0]?.photo} />
             <div className='sidebarThread__details'>
-                <h3>Thread name</h3>
-                <p>This is the info</p>
-                <small className='sidebarThread__timestamp'>timestap</small>
+                <h3>{threadName}</h3>
+                {/* <p>{threadInfo[0]?.message}</p> */}
+                <p>Hello</p>
+                <small className='sidebarThread__timestamp'>
+                    {/* {new Date(threadInfo[0]?.timestamp?.toDate())} */}
+                    timestamp
+                </small>
             </div>
         </div>
     )
